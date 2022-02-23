a3517dcda57d64300be6244f838ea72ba1f775b8
dwayneRichards <dwaynerichards@gmail.com>
2022-02-22 21:34:16 -0500

    all tests passing

--- packages/hardhat-ts/contracts/Vendor.sol
+++ packages/hardhat-ts/contracts/Vendor.sol
@@ -8,26 +8,36 @@ contract Vendor is Ownable {
   uint256 constant tokensPerEth = 100;
   mapping(address => uint256) tokenBalances;
   mapping(address => bool) hasPurchased;
+  bool locked = true;
 
   constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
   }
 
-  event BuyToken(address buyer, uint256 amountOfEth, uint256 amountOfTokens);
+  event BuyTokens(address buyer, uint256 amountOfEth, uint256 amountOfTokens);
+  event SellTokens(address seller, uint256 amountOfEth, uint256 amountOfTokens);
 
   function buyTokens() external payable {
+    require(locked, 'Transaction not allowed while locked');
+    locked = false;
     uint256 etherSent = convertUnit('toEther', msg.value);
     uint256 tokensToSend = tokensPerEth * etherSent;
     bool purchaser = hasPurchased[msg.sender];
     if (purchaser == false) purchaser = true;
     tokenBalances[msg.sender] += tokensToSend;
-    yourToken.transfer(msg.sender, tokensToSend);
-    emit BuyToken(msg.sender, etherSent, tokensToSend);
+    emit BuyTokens(msg.sender, etherSent, tokensToSend);
+    bool success = yourToken.transfer(msg.sender, tokensToSend);
+    uint256 buyerTokens = yourToken.balanceOf(msg.sender);
+    console.log('%s wei converted to %s ether', msg.value, etherSent);
+    console.log('%s account purchased %s tokens for %s ether', msg.sender, tokensToSend, etherSent);
+    console.log('%s has %s tokens post transfer: ', msg.sender, buyerTokens);
+    locked = true;
+    require(success, 'tranfer of tokens not successfull');
   }
 
   function convertUnit(string memory _unit, uint256 _amount) private pure returns (uint256) {
-    uint256 _ether = _amount * 10**18;
-    uint256 _wei = _amount / 10**18;
+    uint256 _ether = _amount / 10**18;
+    uint256 _wei = _amount * 10**18;
     if (compareStrings(_unit, 'toWei')) return _wei;
     if (compareStrings(_unit, 'toEther')) return _ether;
   }
@@ -44,20 +54,20 @@ contract Vendor is Ownable {
     require(success, 'withdrawl not successfull');
   }
 
+  modifier canSell(address _seller) {
+    bool _canSell = hasPurchased[_seller];
+    uint256 _tokenBalance = tokenBalances[_seller];
+    require(_canSell, 'Must have purchased tokens');
+    require(_tokenBalance > 0, 'Must have tokens');
+    _;
+  }
+
   modifier vendApproveToken(uint256 _amount) {
     bool _success = yourToken.approve(address(this), _amount);
     //emits Approval(address owner, address spender, uint value)
     //Approval(yourToken.address, msg.sender, _amount)
     console.log('Approval emitted with tokenContractAddress: %s, msgSpender: %s, value: %s', address(yourToken), msg.sender, _amount);
     require(_success, '!!Vendor Not Approved!!');
-    _;
-  }
-
-  modifier canSell(address _seller) {
-    bool _canSell = hasPurchased[_seller];
-    uint256 _tokenBalance = tokenBalances[_seller];
-    require(_canSell, 'Must have purchased tokens');
-    require(_tokenBalance > 0, 'Must have tokens');
     _;
   }
 
@@ -68,15 +78,12 @@ contract Vendor is Ownable {
     console.log('Transfer emitted from: %s, tp: %s, amount: %s', msg.sender, address(this), _tokens);
   }
 
-  // ToDo: create a sellTokens() function:
-  //user first must call yourToken.approve(address(this), amountOfTokens), returtns bool
-  //then ourToken must transferFrom the msg.sender to THIS address, the amountOfTokens
-  //ethToSend = tokenAmount * tokens per ether
   function sellToken(uint256 _tokenAmount) external vendApproveToken(_tokenAmount) canSell(msg.sender) {
     vendorTransfer(_tokenAmount);
     uint256 ethToSend = _tokenAmount / tokensPerEth;
     uint256 weiToSend = convertUnit('toWei', ethToSend);
     //using .call rather than .send in the event of nonStandard gas fees
+    emit SellTokens(msg.sender, ethToSend, _tokenAmount);
     (bool success, ) = payable(msg.sender).call{value: weiToSend}('');
     require(success, 'Sale not successful.');
   }
--- packages/hardhat-ts/contracts/YourToken.sol
+++ packages/hardhat-ts/contracts/YourToken.sol
@@ -6,17 +6,19 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 
 import 'hardhat/console.sol';
 
-// learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
-
 contract YourToken is ERC20, Ownable {
   address public frontend = 0x88E0c097d8e20FDafb05bF419CF60Cf8233F72f0;
 
   constructor(uint256 _initialSupply) ERC20('Gold', 'GLD') {
     uint256 initialSupply = _initialSupply * 10**18;
     _mint(msg.sender, initialSupply);
-    //(bool success, ) = payable(frontend).call{value: initialSupply}('');
     uint256 _totalSupply = totalSupply();
-    console.log('inial supply sent to %s with a total supply of %s', frontend, _totalSupply);
-    //require(success, 'tokens not transfered');
+    bool equalTotalSupply = (_totalSupply == 1000 ether);
+    console.log('total supply is 1000 ether: %s', equalTotalSupply);
+    uint256 ownerBalance = balanceOf(owner());
+    bool ownerHasFullBalance = (ownerBalance == totalSupply());
+    console.log('***ownerBalance***: %s: ', ownerBalance);
+    require(msg.sender == owner(), 'MsgSender not Owner');
+    require(ownerHasFullBalance, 'Balance not minted to owner');
   }
 }
